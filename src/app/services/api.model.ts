cbf56c8b43a60906a162f8386dbf3a44a9edbdac
Simon Moreau <simon@bim42.com>
2020-11-12 16:26:23 +0100

    add api model

--- src/app/services/api.model.ts
+++ src/app/services/api.model.ts
@@ -1,5 +1,29 @@
 export interface IForgeToken {
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-  }
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+}
+
+export interface RvtFile {
+  url: string;
+}
+
+export interface Param {
+  url: string;
+}
+
+export interface Result {
+  verb: string;
+  url: string;
+}
+
+export interface Arguments {
+  rvtFile: RvtFile;
+  param: Param;
+  result: Result;
+}
+
+export interface WorkItemDescription {
+  activityId: string;
+  arguments: Arguments;
+}
