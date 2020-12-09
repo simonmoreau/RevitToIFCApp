a4225b40b4dfd0703c6f75c2512fef6a9afc5d13
Simon Moreau <simon@bim42.com>
2020-12-09 21:18:53 +0100

    improve the logo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,34 +5,44 @@ import { MsalService } from '@azure/msal-angular';
 import { ApiService } from '../services/api.service';
 
 import { ForgeService } from '../forge/forge.service';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-
-
 export class HomeComponent implements OnInit {
-
   profile: any;
   callContent: string;
   rootURL = '/api';
 
-  constructor(private authService: MsalService, private apiService: ApiService, private http: HttpClient) { }
+  constructor(
+    private authService: MsalService,
+    private apiService: ApiService,
+    private http: HttpClient,
+    iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer
+  ) {
+    iconRegistry.addSvgIcon(
+      'thumbs-up',
+      sanitizer.bypassSecurityTrustResourceUrl(
+        'assets/img/examples/thumbup-icon.svg'
+      )
+    );
+  }
 
   ngOnInit() {
     this.profile = this.authService.getAccount();
   }
 
-  onForgeCall(){
+  onForgeCall() {
     // this.forgeService.getActivities().subscribe(a => this.callContent = JSON.stringify(a));
 
-    this.apiService.getLocalAPI().subscribe(a => this.callContent = a.name);
+    this.apiService.getLocalAPI().subscribe((a) => (this.callContent = a.name));
 
     // this.getTasks().subscribe(r => console.log(r));
-
-
   }
 
   getTasks() {
