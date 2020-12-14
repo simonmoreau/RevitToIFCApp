107949c48c5e509aabcdc8f0b89e6ad3289d798f
Simon Moreau <simon@bim42.com>
2020-12-14 17:04:31 +0100

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  localUri: 'https://green-beach-046b3a203.azurestaticapps.net',
+  localUri: 'https://icy-sky-095454703.azurestaticapps.net',
   apiUri: '',
   stripe_key: 'pk_test_51HKjR0FjsZIqAFNU21RxcBqZraZIoqa7RBvmBefDK04IGqKvYFIOPlGgm3qOQth5zimpmFmuvfFjryOU4gl8yqun00Gjcq2Ot7'
 };
