0659c613b92d0a47fbb4fcda05235141ad4e4c41
Simon Moreau <simon@bim42.com>
2020-12-14 17:30:03 +0100

    add error handling to the functions

--- api/CreateCheckoutSession.cs
+++ api/CreateCheckoutSession.cs
@@ -13,48 +13,56 @@ using System.Collections.Generic;
 
 namespace api
 {
-    public static class CreateCheckoutSession
+  public static class CreateCheckoutSession
+  {
+    [FunctionName("CreateCheckoutSession")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "checkoutSession")] HttpRequest req,
+        [Queue("orders", Connection = "StorageConnectionString")] IAsyncCollector<Order> orderQueue,
+        ILogger log)
     {
-        [FunctionName("CreateCheckoutSession")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "checkoutSession")] HttpRequest req,
-            [Queue("orders", Connection = "StorageConnectionString")] IAsyncCollector<Order> orderQueue,
-            ILogger log)
+      try
+      {
+        log.LogInformation("C# HTTP trigger function processed the checkoutSession request.");
+
+        StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("stripe_api_key");
+
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        CheckoutBody checkoutBody = JsonConvert.DeserializeObject<CheckoutBody>(requestBody);
+
+        SessionLineItemOptions product = new SessionLineItemOptions
         {
-            log.LogInformation("C# HTTP trigger function processed the checkoutSession request.");
+          Price = checkoutBody.productId,
+          Quantity = 1,
+        };
 
-            StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("stripe_api_key");
+        string localUri = Environment.GetEnvironmentVariable("local_uri");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            CheckoutBody checkoutBody = JsonConvert.DeserializeObject<CheckoutBody>(requestBody);
+        SessionCreateOptions options = new SessionCreateOptions
+        {
+          PaymentMethodTypes = new List<string> { "card", },
+          LineItems = new List<SessionLineItemOptions> { product },
+          Mode = "payment",
+          SuccessUrl = localUri + "checkout/success?session_id={CHECKOUT_SESSION_ID}",
+          CancelUrl = localUri + "checkout/cancel",
+        };
 
-            SessionLineItemOptions product = new SessionLineItemOptions
-            {
-                Price = checkoutBody.productId,
-                Quantity = 1,
-            };
+        SessionService service = new SessionService();
+        Session session = service.Create(options);
 
-            string localUri = Environment.GetEnvironmentVariable("local_uri");
+        return new OkObjectResult(new { id = session.Id });
+      }
+      catch (Exception ex)
+      {
+        return new BadRequestObjectResult(ex);
+      }
 
-            SessionCreateOptions options = new SessionCreateOptions
-            {
-                PaymentMethodTypes = new List<string> { "card", },
-                LineItems = new List<SessionLineItemOptions> { product },
-                Mode = "payment",
-                SuccessUrl = localUri + "checkout/success?session_id={CHECKOUT_SESSION_ID}",
-                CancelUrl = localUri + "checkout/cancel",
-            };
-
-            SessionService service = new SessionService();
-            Session session = service.Create(options);
-
-            return new OkObjectResult(new { id = session.Id });
-        }
     }
+  }
 
-    public class CheckoutBody
-    {
-        public string userId { get; set; }
-        public string productId { get; set; }
-    }
+  public class CheckoutBody
+  {
+    public string userId { get; set; }
+    public string productId { get; set; }
+  }
 }
--- api/GetConversionsNumber.cs
+++ api/GetConversionsNumber.cs
@@ -20,45 +20,54 @@ namespace api
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "token")] HttpRequest req,
         ILogger log)
     {
-      string userId = req.Query["id"];
 
-      log.LogInformation("C# HTTP trigger function processed the UpdateConversionToken request.");
-
-      // Initialize the client credential auth provider
-      IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
-          .Create(Environment.GetEnvironmentVariable("AppId"))
-          .WithTenantId(Environment.GetEnvironmentVariable("TenantId"))
-          .WithClientSecret(Environment.GetEnvironmentVariable("ClientSecret"))
-          .Build();
-      ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
-
-      // Set up the Microsoft Graph service client with client credentials
-      GraphServiceClient graphClient = new GraphServiceClient(authProvider);
-
-      string b2cExtensionAppClientId = Environment.GetEnvironmentVariable("b2cExtensionAppClientId");
-
-      // Declare the names of the custom attributes
-      const string customAttributeName = "ConversionToken";
-
-      // Get the complete name of the custom attribute (Azure AD extension)
-      B2cCustomAttributeHelper helper = new B2cCustomAttributeHelper(b2cExtensionAppClientId);
-      string ConversionTokenAttributeName = helper.GetCompleteAttributeName(customAttributeName);
-
-      // Get all users (one page)
-      User existingUser = await graphClient.Users[userId]
-          .Request()
-          .Select($"id,displayName,identities,{ConversionTokenAttributeName}")
-          .GetAsync();
-
-      int existingToken = 0;
-
-      if (existingUser.AdditionalData.ContainsKey(ConversionTokenAttributeName))
+      try
       {
-        object exisitingTokenObject = existingUser.AdditionalData[ConversionTokenAttributeName];
-        int.TryParse(exisitingTokenObject.ToString(), out existingToken);
-      }
+        string userId = req.Query["id"];
 
-      return new OkObjectResult(new { userId = userId, tokenNumber = existingToken });
+        log.LogInformation("C# HTTP trigger function processed the UpdateConversionToken request.");
+
+        // Initialize the client credential auth provider
+        IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+            .Create(Environment.GetEnvironmentVariable("AppId"))
+            .WithTenantId(Environment.GetEnvironmentVariable("TenantId"))
+            .WithClientSecret(Environment.GetEnvironmentVariable("ClientSecret"))
+            .Build();
+        ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+
+        // Set up the Microsoft Graph service client with client credentials
+        GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+
+        string b2cExtensionAppClientId = Environment.GetEnvironmentVariable("b2cExtensionAppClientId");
+
+        // Declare the names of the custom attributes
+        const string customAttributeName = "ConversionToken";
+
+        // Get the complete name of the custom attribute (Azure AD extension)
+        B2cCustomAttributeHelper helper = new B2cCustomAttributeHelper(b2cExtensionAppClientId);
+        string ConversionTokenAttributeName = helper.GetCompleteAttributeName(customAttributeName);
+
+        // Get all users (one page)
+        User existingUser = await graphClient.Users[userId]
+            .Request()
+            .Select($"id,displayName,identities,{ConversionTokenAttributeName}")
+            .GetAsync();
+
+        int existingToken = 0;
+
+        if (existingUser.AdditionalData.ContainsKey(ConversionTokenAttributeName))
+        {
+          object exisitingTokenObject = existingUser.AdditionalData[ConversionTokenAttributeName];
+          int.TryParse(exisitingTokenObject.ToString(), out existingToken);
+        }
+
+        return new OkObjectResult(new { userId = userId, tokenNumber = existingToken });
+      }
+      catch (Exception ex)
+      {
+
+        return new BadRequestObjectResult(ex);
+      }
     }
   }
 }
