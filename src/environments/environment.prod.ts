08bfbc9d65424e898725c822d29c80ca0ec0a8b4
Simon Moreau <simon@bim42.com>
2020-12-21 18:33:58 +0100

    update project for new deployement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  localUri: 'https://icy-sky-095454703.azurestaticapps.net',
-  apiUri: '',
+  localUri: 'https://delightful-pebble-01f2a2d03.azurestaticapps.net',
+  apiUri: 'https://revittoifcfunction.azurewebsites.net',
   stripe_key: 'pk_test_51HKjR0FjsZIqAFNU21RxcBqZraZIoqa7RBvmBefDK04IGqKvYFIOPlGgm3qOQth5zimpmFmuvfFjryOU4gl8yqun00Gjcq2Ot7'
 };
