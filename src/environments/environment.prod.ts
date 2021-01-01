ad8f45c48874d020aa9f9b0ba1f386d7fc32ae25
Simon Moreau <simon@bim42.com>
2021-01-01 21:55:41 +0100

    change domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  localUri: 'https://delightful-pebble-01f2a2d03.azurestaticapps.net',
+  localUri: 'https://rvt-to-ifc.bim42.com',
   apiUri: 'https://revittoifcfunction.azurewebsites.net',
   stripe_key: 'pk_live_51HKjR0FjsZIqAFNU0NBFRDkiGrWtBanJGr2hsJcuvSPQfXI5390Kg115XDyvmLKuJqBPPJ85JKcS0HDkzW87gIlx00qpjJHlsE'
 };
