ed79c96bce392b663adf6a7fa0a78d78332f5123
Simon Moreau <simon@bim42.com>
2024-07-16 11:58:24 +0200

    Add checkout page

--- src/WebClient/Pages/Home.razor.cs
+++ src/WebClient/Pages/Home.razor.cs
@@ -2,15 +2,7 @@
 {
     public partial class Home
     {
-        public string Text { get; set; } = "????";
-        public string ButtonText { get; set; } = "Click Me";
-        public int ButtonClicked { get; set; }
 
-        void ButtonOnClick()
-        {
-            ButtonClicked += 1;
-            Text = $"Awesome x {ButtonClicked}";
-            ButtonText = "Click Me Again";
-        }
+
     }
 }
