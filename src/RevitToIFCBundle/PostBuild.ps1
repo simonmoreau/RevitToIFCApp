38eaf819993931d510aee1bea1744457d7ec50b2
Simon Moreau <simon@bim42.com>
2024-08-16 11:16:06 +0200

    Update post build

--- src/RevitToIFCBundle/PostBuild.ps1
+++ src/RevitToIFCBundle/PostBuild.ps1
@@ -1,18 +1,39 @@
-﻿param ($Configuration, $TargetName, $ProjectDir, $TargetPath, $TargetDir)
-write-host $Configuration
+﻿param ($RevitVersion, $TargetName, $ProjectDir, $TargetPath, $TargetDir)
+write-host $RevitVersion
 write-host $TargetName
 write-host $ProjectDir
 write-host $TargetPath
 write-host $TargetDir
 
-# sign the dll
-$thumbPrint = "e729567d4e9be8ffca04179e3375b7669bccf272"
-$cert=Get-ChildItem -Path Cert:\CurrentUser\My -CodeSigningCert | Where { $_.Thumbprint -eq $thumbPrint}
+$filePaths = @()
 
-Set-AuthenticodeSignature -FilePath $TargetPath -Certificate $cert -IncludeChain All -TimestampServer "http://timestamp.comodoca.com/authenticode"
+function SignFiles($TargetDir) {
+    Get-ChildItem ($TargetDir) -Recurse | Where-Object { $_.extension -in ".exe", ".dll" } |
+    Foreach-Object {
+
+        $signature = Get-AuthenticodeSignature -FilePath $_.FullName
+
+        if ($signature.status -ne "Valid") {
+            # $filePaths = $filePaths + " " + "`"" + $_.FullName +"`""
+            $filePaths += $_.FullName
+        }
+    }
+
+    Write-Host $filePaths
+
+    azuresigntool sign -du "https://www.eai.fr" `
+      -fd sha384 -kvu https://kv-eai-util-fr-signing.vault.azure.net/ `
+      -kvm `
+      -kvc EAICodeSigningCertificate `
+      -tr http://timestamp.digicert.com `
+      -td sha384 `
+      -v $filePaths
+}
+
+SignFiles $TargetDir
 
 # Copy to Addin folder for debug
-$addinFolder = ($env:APPDATA + "\Autodesk\REVIT\Addins\2025")
+$addinFolder = ($env:APPDATA + "\Autodesk\REVIT\Addins\" + $RevitVersion)
 xcopy /Y /F ($TargetPath) ($addinFolder)
 xcopy /Y /F ($ProjectDir + "RevitToIFCBundle.bundle\Contents\RevitToIFCBundle.addin") ($addinFolder)
 
@@ -23,7 +44,7 @@ xcopy /Y /F ($TargetPath) ($BundleFolder + "Contents\")
 
 ## Zip the package
 $ReleasePath = $TargetDir
-$ReleaseZip = ($ReleasePath + "\" + $TargetName + ".zip")
+$ReleaseZip = ($ReleasePath + "\" + $TargetName + $RevitVersion + ".zip")
 
 if (Test-Path $ReleaseZip) { Remove-Item $ReleaseZip }
 
