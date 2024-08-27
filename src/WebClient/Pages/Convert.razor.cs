9887e7cf545007b639bfab1af3c43188718eca02
Simon Moreau <simon@bim42.com>
2024-08-27 16:58:55 +0200

    Small fixes

--- src/WebClient/Pages/Convert.razor.cs
+++ src/WebClient/Pages/Convert.razor.cs
@@ -1,6 +1,24 @@
-﻿namespace WebClient.Pages
+﻿using Microsoft.AspNetCore.Components;
+using WebClient.Models;
+using WebClient.Services;
+
+namespace WebClient.Pages
 {
     public partial class Convert
     {
+        [Inject]
+        public IDataService _dataService { get; set; }
+
+        UserDTO User { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (User == null)
+            {
+                User = await _dataService.GetMe();
+            }
+            
+
+        }
     }
 }
